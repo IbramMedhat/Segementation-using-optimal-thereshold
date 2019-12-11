355e86d345560469a4cf0c2c716f4060f98f5969
unknown <ibram.medhat2@gmail.com>
2019-12-11 18:48:46 +0200

    generating the gray scale segmented image

--- Segmentation-Computer-Vision.py
+++ Segmentation-Computer-Vision.py
@@ -29,6 +29,14 @@ def recalculate_theresholds(means_array) :
         thereshold_array[i] = (means_array[i] + means_array[i+1]) / 2
     return thereshold_array
 
+
+def generate_gray_segmented_image(object_detection_img, thereshold_array) :
+    gray_image_array = object_detection_img
+    for x in range(object_detection_img.shape[0]) :
+        for y in range(object_detection_img.shape[1]) :
+            gray_image_array[x][y] = object_detection_img[x][y] * (255 / thereshold_array.size+1)
+    return gray_image_array
+
 #The function should take as inputs the image to be
 #segmented and n. The function should return the computed n thresholds, a binary image for each segment
 #and one segmented gray-scale image with each segment assigned a different gray-level
@@ -67,9 +75,15 @@ def detect_objects(org_image, n) :
         else :
             means_array = new_means_array
             thereshold_array = recalculate_theresholds(means_array)
-
+    
+    segmented_image_array = generate_gray_segmented_image(object_detection_img, thereshold_array)
+    segmented_image = Image.fromarray(segmented_image_array)
+    segmented_image = segmented_image.convert("L")
+    return segmented_image
+    
 image_filepath = "GUC.jpg"
 org_image = Image.open(image_filepath)
-detect_objects(org_image, 1)                
+segmented_image = detect_objects(org_image, 2)                
+segmented_image.save(image_filepath + "segmented image.jpg")
 
 #detect_objects(None,4)

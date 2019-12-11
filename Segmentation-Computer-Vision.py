5d2655cf570ed70425342b321e7f444f92dc3d74
unknown <ibram.medhat2@gmail.com>
2019-12-11 16:43:38 +0200

    Adding initial implementation with random initiazliztion for the theresholds

--- Segmentation-Computer-Vision.py
+++ Segmentation-Computer-Vision.py
@@ -5,4 +5,63 @@ Created on Wed Dec  4 14:20:38 2019
 @author: Ibram Medhat, Basem Rizk
 """
 
+import random
+import numpy as np
+from PIL import Image
 
+
+def recalculate_means(org_image_array, object_detection_img, n) :
+    count_each_segment = np.zeros(n+1)
+    means_array = np.zeros(n+1)
+    for x in range(org_image_array.shape[0]) :
+        for y in range(org_image_array.shape[1]) :
+            means_array[int(object_detection_img[x][y])] += org_image_array[x][y]
+            count_each_segment[int(object_detection_img[x][y])] += 1
+    for i in range(n+1) :
+        means_array[i] = means_array[i] / count_each_segment[i]
+    return means_array
+
+
+
+#The function should take as inputs the image to be
+#segmented and n. The function should return the computed n thresholds, a binary image for each segment
+#and one segmented gray-scale image with each segment assigned a different gray-level
+def detect_objects(org_image, n) :
+    thereshold_array = np.zeros(n)
+    means_array = np.zeros(n+1)
+    #Using Random initialization for n theresholds
+    for i in range(n) :
+        thereshold_array[i] = random.randint(0,255)
+    print(thereshold_array)
+    org_image_array = np.array(org_image)
+    object_detection_img = np.zeros(org_image_array.shape[0]*org_image_array.shape[1]).reshape(org_image_array.shape[0], org_image_array.shape[1])
+    
+    means_changed = True
+    
+    while(means_changed) :
+    
+        #grouping the orginal image into different groups according to the theresholds values
+        for i in range(n) :
+            for x in range(org_image_array.shape[0]) :
+                for y in range(org_image_array.shape[1]) :
+                    if(i == 0) :
+                        if(org_image_array[x][y] < thereshold_array[i]) :
+                            object_detection_img[x][y] = i
+                    else : 
+                        if(org_image_array[x][y] < thereshold_array[i] and org_image_array[x][y] > thereshold_array[i]) :
+                            object_detection_img[x][y] = i
+                    if(org_image_array[x][y] > thereshold_array[n-1]) :
+                        object_detection_img[x][y] = n
+        new_means_array = recalculate_means(org_image_array, object_detection_img, n)
+        print(new_means_array)
+        if(np.array_equal(means_array,new_means_array)) :
+            means_changed = False
+        else :
+            means_array = new_means_array
+        
+
+image_filepath = "GUC.jpg"
+org_image = Image.open(image_filepath)
+detect_objects(org_image, 1)                
+
+#detect_objects(None,4)

f7888b6059c9ca3c6fd0cb820f894b479c8cae9e
Basem Rizk <basem.rizk@outlook.com>
2019-12-13 02:33:58 +0200

    Extract images function implemented + finalizing assignment + a bit refactoring

--- Segmentation-Computer-Vision.py
+++ Segmentation-Computer-Vision.py
@@ -2,16 +2,23 @@
 """
 Created on Wed Dec  4 14:20:38 2019
 
-@author: Ibram Medhat, Basem Rizk
+@authors: Ibram Medhat, Basem Rizk
 """
 
 import random
 import numpy as np
 from PIL import Image
 
+def print_progress(iteration_type, iteration_value, end_value = 0, upper_bound_exist = False):
+    if(upper_bound_exist):
+        iteration_value = np.around((iteration_value/end_value)*100,
+                                    decimals = 1)
+    print( '\r ' + iteration_type + ' %s' % (str(iteration_value)),
+              end = '\r')
+
 #Calculates the means of the different segments in the image based on the theresholds
 #and pixels in each segement
-def recalculate_means(org_image_array, object_detection_img, n) :
+def recalculate_means(org_image_array, object_detection_img, n):
     count_each_segment = np.zeros(n+1)
     means_array = np.zeros(n+1)
     for x in range(org_image_array.shape[0]) :
@@ -38,53 +45,106 @@ def generate_gray_segmented_image(object_detection_img, thereshold_array) :
     return gray_image_array
 
 #The function should take as inputs the image to be
-#segmented and n. The function should return the computed n thresholds, a binary image for each segment
+#segmented and n. The function should return the computed n thresholds, 
+# a binary image for each segment
 #and one segmented gray-scale image with each segment assigned a different gray-level
-def detect_objects(org_image, n) :
-    thereshold_array = np.zeros(n)
+def detect_objects(org_image_array, n) :
+    def random_thresholds(num_of_thresholds):
+        #Using Random initialization for n theresholds
+        threshold_array = np.zeros((num_of_thresholds,))
+        for i in range(num_of_thresholds) :
+            threshold_array[i] = random.randint(0,255)
+        return np.sort(threshold_array)
+ 
     means_array = np.zeros(n+1)
-    #Using Random initialization for n theresholds
-    for i in range(n) :
-        thereshold_array[i] = random.randint(0,255)
-    thereshold_array = np.sort(thereshold_array,axis=None)
-    print(thereshold_array)
-    org_image_array = np.array(org_image)
-    object_detection_img = np.zeros(org_image_array.shape[0]*org_image_array.shape[1]).reshape(org_image_array.shape[0], org_image_array.shape[1])
+    threshold_array = random_thresholds(n)
+    print("Init thresholds: " + str(threshold_array))
+    object_detection_img =\
+        np.zeros(org_image_array.shape[0]*\
+                 org_image_array.shape[1]).reshape(org_image_array.shape[0],
+                                      org_image_array.shape[1])
     
     means_changed = True
     
+    iteration = 1
     while(means_changed) :
-    
+        print_progress("iteration: ", iteration)
+        iteration+=1
+        
         #grouping the orginal image into different groups according to the theresholds values
         for i in range(n) :
             for x in range(org_image_array.shape[0]) :
                 for y in range(org_image_array.shape[1]) :
                     if(i == 0) :
-                        if(org_image_array[x][y] < thereshold_array[i]) :
+                        if(org_image_array[x][y] < threshold_array[i]) :
                             object_detection_img[x][y] = i
                     else : 
-                        if(org_image_array[x][y] < thereshold_array[i] and org_image_array[x][y] > thereshold_array[i-1]) :
+                        if(org_image_array[x][y] < threshold_array[i]\
+                           and org_image_array[x][y] > threshold_array[i-1]) :
                             object_detection_img[x][y] = i
                             #print("here")
-                    if(org_image_array[x][y] > thereshold_array[n-1]) :
+                    if(org_image_array[x][y] > threshold_array[n-1]) :
                         object_detection_img[x][y] = n    
+                        
         new_means_array = recalculate_means(org_image_array, object_detection_img, n)
-        print(new_means_array) 
+#        print(new_means_array) 
+        
         if(np.array_equal(means_array,new_means_array)) :
             means_changed = False
         else :
             means_array = new_means_array
-            thereshold_array = recalculate_theresholds(means_array)
-    
-    segmented_image_array = generate_gray_segmented_image(object_detection_img, thereshold_array)
+            threshold_array = recalculate_theresholds(means_array)
+    print()
+    print("Converged.")
+    print("Final thresholds: " + str(threshold_array))
+
+    segmented_image_array = generate_gray_segmented_image(object_detection_img,
+                                                          threshold_array)
     segmented_image = Image.fromarray(segmented_image_array)
     segmented_image = segmented_image.convert("L")
-    return segmented_image, thereshold_array
+    return segmented_image, threshold_array
+   
+    
+def extract_objects(org_img_array, threshold_array):
+    all_objects = np.zeros((threshold_array.shape[0] + 1,
+                            org_img_array.shape[0],
+                            org_img_array.shape[1]))
+    all_images = []
+    full_threshold_array =\
+        np.concatenate((np.array([0]), threshold_array, np.array([255])))
+    for i in range(full_threshold_array.shape[0] - 1):    
+        all_objects[i] = 255*\
+                        ((org_img_array > full_threshold_array[i])*\
+                         (org_img_array < full_threshold_array[i+1]))
+                
+        image = Image.fromarray(all_objects[i])
+        image = image.convert("L")
+        all_images.append(image)
+        
+    return all_images, all_objects    
+    
+# =============================================================================
+# ------Application
+# =============================================================================
+def segment(org_img_array, num_of_thresholds, image_filepath):
+    print("Segement using Optimal Thresholding with n = " + str(num_of_thresholds))
+    
+    # Apply Optimal-thesholding-segementation
+    segmented_image, threshold_array = detect_objects(org_img_array, num_of_thresholds)
+    segmented_image.save(image_filepath + "_" + str(num_of_thresholds) + ".jpg")
+    
+    # Extract binary images based on computed segmentations
+    all_images, all_objects = extract_objects(org_img_array, threshold_array)
+    for i in range(len(all_images)):
+        all_images[i]\
+            .save(image_filepath + "_" + str(num_of_thresholds) + "_" + str(i+1) + ".jpg")
+        print("Saved image of object " + str(i+1))
+    print("Done.")
+    
     
 image_filepath = "GUC"
 org_image = Image.open(image_filepath + ".jpg")
-segmented_image, thereshold_array = detect_objects(org_image, 3)
-print(thereshold_array)                
-segmented_image.save(image_filepath + "_segmented image.jpg")
+org_img_array = np.array(org_image)
 
-#detect_objects(None,4)
+segment(org_img_array, 3, image_filepath)
+segment(org_img_array, 4, image_filepath)
